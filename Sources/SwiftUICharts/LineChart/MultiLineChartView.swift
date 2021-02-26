f3ded06218bab2119363a694eb55d742c6817d67
Simon Ammer <ammersimon@gmail.com>
2021-02-26 12:10:09 +0100

    Remove rounded rectangle from MultiLineChartView

--- Sources/SwiftUICharts/LineChart/MultiLineChartView.swift
+++ Sources/SwiftUICharts/LineChart/MultiLineChartView.swift
@@ -68,96 +68,42 @@ public struct MultiLineChartView: View {
     }
     
     public var body: some View {
-        ZStack(alignment: .center){
-            RoundedRectangle(cornerRadius: 20)
-                .fill(self.colorScheme == .dark ? self.darkModeStyle.backgroundColor : self.style.backgroundColor)
-                .frame(width: frame.width, height: 240, alignment: .center)
-                .shadow(radius: self.dropShadow ? 8 : 0)
-            VStack(alignment: .leading){
-                if(!self.showIndicatorDot){
-                    VStack(alignment: .leading, spacing: 8){
-                        Text(self.title)
-                            .font(.title)
-                            .bold()
-                            .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
-                        if (self.legend != nil){
-                            Text(self.legend!)
-                                .font(.callout)
-                                .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor : self.style.legendTextColor)
-                        }
-                        HStack {
-                            if (rateValue ?? 0 >= 0){
-                                Image(systemName: "arrow.up")
-                            }else{
-                                Image(systemName: "arrow.down")
-                            }
-                            Text("\(rateValue ?? 0)%")
-                        }
-                    }
-                    .transition(.opacity)
-                    .animation(.easeIn(duration: 0.1))
-                    .padding([.leading, .top])
-                }else{
-                    HStack{
-                        Spacer()
-                        Text("\(self.currentValue, specifier: self.valueSpecifier)")
-                            .font(.system(size: 41, weight: .bold, design: .default))
-                            .offset(x: 0, y: 30)
-                        Spacer()
-                    }
-                    .transition(.scale)
-                }
-                Spacer()
-                GeometryReader{ geometry in
-                    ZStack{
-                        ForEach(0..<self.data.count) { i in
-                            Line(data: self.data[i],
-                                 frame: .constant(geometry.frame(in: .local)),
-                                 touchLocation: self.$touchLocation,
-                                 showIndicator: self.$showIndicatorDot,
-                                 minDataValue: .constant(self.globalMin),
-                                 maxDataValue: .constant(self.globalMax),
-                                 showBackground: false,
-                                 gradient: self.data[i].getGradient(),
-                                 index: i)
-                        }
+        ZStack(alignment: .center) {
+            Spacer()
+            GeometryReader{ geometry in
+                ZStack{
+                    ForEach(0..<self.data.count) { i in
+                        Line(data: self.data[i],
+                             frame: .constant(geometry.frame(in: .local)),
+                             touchLocation: self.$touchLocation,
+                             showIndicator: self.$showIndicatorDot,
+                             minDataValue: .constant(self.globalMin),
+                             maxDataValue: .constant(self.globalMax),
+                             showBackground: false,
+                             gradient: self.data[i].getGradient(),
+                             index: i)
                     }
                 }
-                .frame(width: frame.width, height: frame.height + 30)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .offset(x: 0, y: 0)
-            }.frame(width: self.formSize.width, height: self.formSize.height)
+            }
+            .frame(width: frame.width, height: frame.height + 30)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .offset(x: 0, y: 0)
         }
+        .frame(width: self.formSize.width, height: self.formSize.height)
         .gesture(DragGesture()
-        .onChanged({ value in
-//            self.touchLocation = value.location
-//            self.showIndicatorDot = true
-//            self.getClosestDataPoint(toPoint: value.location, width:self.frame.width, height: self.frame.height)
-        })
-            .onEnded({ value in
-                self.showIndicatorDot = false
-            })
+                    .onChanged({ value in
+                    })
+                    .onEnded({ value in
+                        self.showIndicatorDot = false
+                    })
         )
     }
-    
-//    @discardableResult func getClosestDataPoint(toPoint: CGPoint, width:CGFloat, height: CGFloat) -> CGPoint {
-//        let points = self.data.onlyPoints()
-//        let stepWidth: CGFloat = width / CGFloat(points.count-1)
-//        let stepHeight: CGFloat = height / CGFloat(points.max()! + points.min()!)
-//
-//        let index:Int = Int(round((toPoint.x)/stepWidth))
-//        if (index >= 0 && index < points.count){
-//            self.currentValue = points[index]
-//            return CGPoint(x: CGFloat(index)*stepWidth, y: CGFloat(points[index])*stepHeight)
-//        }
-//        return .zero
-//    }
 }
 
 struct MultiWidgetView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            MultiLineChartView(data: [([8,23,54,32,12,37,7,23,43], GradientColors.orange)], title: "Line chart", legend: "Basic")
+            MultiLineChartView(data: [([8,23,54,32,12,37,7,23,43], GradientColors.orange),([9,11,40,28,19,30,4,35,27], GradientColors.bluPurpl)], title: "Line chart", legend: "Basic")
                 .environment(\.colorScheme, .light)
         }
     }
